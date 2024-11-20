a676911a9321010faf21d8e399d1092ad4bb1329
Rilwan Idowu Suleiman <easyrilwan@gmail.com>
2024-11-20 09:18:40 +0100

    spacing v2

--- src/components/features/home/CardList.jsx
+++ src/components/features/home/CardList.jsx
@@ -1,7 +1,7 @@
 export default function CardList({ icon: Icon, title, description }) {
   return (
     <div className="group flex items-center gap-2 rounded-3xl border border-aaooGray px-2 py-4 transition-all duration-700 hover:bg-aaooBlack md:gap-4 md:px-4 md:py-8">
-      <span className="rounded-full bg-aaooBlack object-cover p-1 text-xl text-white group-hover:bg-aaooWhite group-hover:text-aaooBlack md:p-2 md:text-[2.5rem]">
+      <span className="rounded-full bg-aaooBlack object-cover p-1 text-xl text-white group-hover:bg-aaooWhite group-hover:text-aaooBlack md:p-2 md:text-3xl lg:text-[2.5rem]">
         <Icon />
       </span>
 
